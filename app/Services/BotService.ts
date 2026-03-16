b79f92d0642da74fac8a0fac8cb561f651706603
Agusteando <desarrollo.tecnologico@casitaiedis.edu.mx>
2026-03-15 22:45:53 -0600

    ts fix

--- app/Services/BotService.ts
+++ app/Services/BotService.ts
@@ -1,4 +1,4 @@
-import { Client, LocalAuth, Message, MessageMedia } from 'whatsapp-web.js'
+import { Client, LocalAuth, Message } from 'whatsapp-web.js'
 import { promises as fs } from 'fs'
 import path from 'path'
 import Application from '@ioc:Adonis/Core/Application'
